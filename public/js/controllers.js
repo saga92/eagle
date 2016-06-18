a74e7763a431021e09e616fb961da4e80b3ab82a
jin1105 <670522325@qq.com>
2016-06-19 03:42:51 +0800

    fix bug

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -16,6 +16,9 @@ angular.module("app.controllers", [ ])
                         signin_username: signInUsername
                     }
                 }).success(function(data){
+                    for(var i=0; i<data.instances.length; ++i){
+                        data.instances[i].container_serial = data.instances[i].container_serial.substring(0,10);
+                    }
                     $scope.instances = data.instances;
                     console.log($scope.instances)
                 });
