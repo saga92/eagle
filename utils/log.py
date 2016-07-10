d6c6b66d25b73507a6b053f5f6c145b53ed471c7
dg8fv2010 <945920259@qq.com>
2016-07-10 18:16:10 +0800

    Revert "Revert "delete comment""
    This reverts commit c6213f5401f94072b80ae4672f82c8566b245dd4.

--- utils/log.py
+++ utils/log.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import logging
-#from logging.handlers import RotatingFileHandler
 from logging.handlers import TimedRotatingFileHandler
 from logging import Formatter
 import imp
@@ -17,7 +16,6 @@ formatter = Formatter(
     '%(asctime)s %(levelname)s: %(message)s '
     '[in %(pathname)s:%(lineno)d]'
 )
-#handler = RotatingFileHandler(app_conf.LOG_PATH + '/eagle.log', maxBytes=10000000, backupCount=2)
 handler = TimedRotatingFileHandler(app_conf.LOG_PATH + '/eagle.log', when="midnight")
 handler.suffix = "%Y-%m-%d"
 handler.setFormatter(formatter)
