3926b2fb81da380c97c1ee5737e32472664a6e19
dg8fv2010 <945920259@qq.com>
2016-07-12 09:34:28 +0800

    merge

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,7 +2,7 @@
 
 
 angular.module("app.controllers", [ ])
-    
+
     .controller("showDashboard", ['$scope', 'Session', '$http', '$window',
         function ($scope, Session, $http, $window) {
             Session.get('is_login', function(res){
@@ -23,14 +23,11 @@ angular.module("app.controllers", [ ])
                         signin_username: signInUsername
                     }
                 }).success(function(data){
-                    for(var i=0; i<data.instances.length; ++i){
-                        data.instances[i].container_serial = data.instances[i].container_serial.substring(0,10);
-                    }
                     $scope.instances = data.instances;
                     console.log($scope.instances)
                 });
             });
-            
+
             $scope.createIns=function(){
                 Session.get("signin_user_name", function(res){
                     var signInUsername = res;
@@ -42,7 +39,6 @@ angular.module("app.controllers", [ ])
                     });
 
                     $http.post(url, parameter).success(function(data){
-                        data.instance.container_serial = data.instance.container_serial.substring(0,10);
                         $scope.instances.push(data.instance);
                         $scope.popup = data.message;
                     }).error(function(data){
--- worker/worker.py
+++ worker/worker.py
@@ -6,8 +6,8 @@ from utils import WorkerQueue
 from utils import worker_logger
 from utils import db
 from model import Instance, User
-import requests
 import worker_cfg
+import docker
 
 def worker_handler(message):
     res = None
@@ -36,40 +36,58 @@ def create_run_container(cli, *args, **kwargs):
     res = {'code': 'error', 'message': 'problem error'}
     image_id = kwargs.get('image_id')
     image_name = worker_cfg.IMAGE_DICT.get(image_id)
-    container = cli.create_container(image=image_name, detach=True, name=kwargs.get('container_name'))
-    response = cli.start(container=container.get('Id'))
-    if response is None:
-        #supposed to be successful
+    try:
+        container = cli.create_container(image=image_name, detach=True, name=kwargs.get('container_name'))
+    except docker.errors.APIError:
+        res['ins'] = {}
+        worker_logger.info("failed to create %s." % kwargs.get('container_name'))
+        return json.dumps(res)
+    else:
         container_serial = container.get('Id')
         inspect_res = cli.inspect_container(container.get('Id'))
         host = inspect_res["NetworkSettings"]["IPAddress"]
+        worker_logger.info("hosts %s." % host)
         port = 22
-        res['code'] = 'ok'
-        res['message'] = 'create successful'
-        res['ins'] = {}
-        res['ins']['image_id'] = kwargs.get('image_id')
-        res['ins']['container_serial'] = container_serial
-        res['ins']['container_name'] = kwargs.get('container_name')
-        res['ins']['host'] = host
-        res['ins']['port'] = port
-        res['ins']['user_name'] = kwargs.get('user_name')
-        res['ins']['status'] = 1
 
         #write db
-        user_query_res = db.session.query(User).filter(\
-                User.username == kwargs.get('user_name')).first()
+        user_query_res = db.session.query(User).filter(User.username == kwargs.get('user_name')).first()
         user_id = user_query_res.id
         ins = Instance(kwargs.get('image_id'), \
-                user_id, kwargs.get('container_name'), container_serial,
-                host, port, 1)
+                       user_id, kwargs.get('container_name'), container_serial,
+                       host, port, 1)
         db.session.add(ins)
         db.session.commit()
-        worker_logger.info("succeed to create %s." % kwargs.get('container_name'))
-    return json.dumps(res)
+        worker_logger.info("succeed to write %s in database." % kwargs.get('container_name'))
+
+        response = cli.start(container=container.get('Id'))
+        if response is None:
+            #supposed to be successful
+            res['code'] = 'ok'
+            res['message'] = 'create successful'
+            res['ins'] = {}
+            res['ins']['image_id'] = kwargs.get('image_id')
+            res['ins']['container_serial'] = container_serial
+            res['ins']['container_name'] = kwargs.get('container_name')
+            res['ins']['host'] = host
+            res['ins']['port'] = port
+            res['ins']['user_name'] = kwargs.get('user_name')
+            res['ins']['status'] = 1
+
+            worker_logger.info("succeed to create %s." % kwargs.get('container_name'))
+        return json.dumps(res)
 
 def stop_container(cli, *args, **kwargs):
     res = {'code': 'error', 'message': 'problem error'}
-    response = cli.stop(kwargs.get('container_serial'))
+    response = None
+    try:
+        response = cli.stop(kwargs.get('container_serial'))
+    except docker.errors.NotFound:
+        res['container_serial'] = kwargs.get('container_serial')
+        instance_query_res = db.session.query(Instance).filter(Instance.container_serial == kwargs.get('container_serial')).first()
+        instance_query_res.status = 5
+        db.session.commit()
+        worker_logger.info("failed to stop %s." % kwargs.get('container_name'))
+
     if response is None:
         #supposed to be successful
         res['code'] = 'ok'
@@ -77,8 +95,7 @@ def stop_container(cli, *args, **kwargs):
         res['container_serial'] = kwargs.get('container_serial')
 
         #write db
-        instance_query_res = db.session.query(Instance).filter(\
-                Instance.container_serial == kwargs.get('container_serial')).first()
+        instance_query_res = db.session.query(Instance).filter(Instance.container_serial == kwargs.get('container_serial')).first()
         instance_query_res.status = 2
         db.session.commit()
         worker_logger.info("succeed to stop %s." % kwargs.get('container_name'))
